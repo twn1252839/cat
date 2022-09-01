0882d21b6c8710a0feb93fe207686196098be648
twn1252839 <107757895+twn1252839@users.noreply.github.com>
2022-09-01 14:39:20 +0800

    新增判斷頁面成功

--- resources/views/cat.blade.php
+++ resources/views/cat.blade.php
@@ -3,15 +3,15 @@
 
 @section('content')
     <div class="container">
+
         <!-- body -->
-        <div class="row">
-            <div class="col-12 title_1">NEWS<span class="title_2">喵星人</span></div>
-            <div class="col title_3">CAT TRIBE</div>
-        </div>
-        <!-- 圖片開始 -->
-        <div class="row">
+        @if (isset($cats))
+            <div class="row">
+                <div class="col-12 title_1">NEWS<span class="title_2">喵星人</span></div>
+                <div class="col title_3">CAT TRIBE</div>
+            </div>
+            <div class="row">
 
-            @if (isset($cats))
                 @foreach ($cats as $cat)
                     <div class="col-4"><img src="{{ asset($cat->pic) }}" class="imgW">
                         <h4> {{ $cat->remark }} </h4><br>
@@ -22,21 +22,44 @@
                         </h5>
                     </div>
                 @endforeach
-            @else
-                @foreach ($cats as $cat)
-                    <div class="col-4"><img src="{{ asset($cat->pic) }}" class="imgW">
-                        <h4> {{ $cat->remark }} </h4><br>
-                        <H6> {{ $cat->aniname }} </H6><br>
+            </div>
+        @elseif (isset($dogs))
+            <div class="row">
+                <div class="col-12 title_1">NEWS<span class="title_2">汪星人</span></div>
+                <div class="col title_3">DOG TRIBE</div>
+            </div>
+            <div class="row">
+
+                @foreach ($dogs as $dog)
+                    <div class="col-4"><img src="{{ asset($dog->pic) }}" class="imgW">
+                        <h4> {{ $dog->remark }} </h4><br>
+                        <H6> {{ $dog->aniname }} </H6><br>
                         <br>
-                        <h5>目前有n位，尚缺n位助養主人<a href=" {{ url('/animals/sponsor/index', $cat->id) }} "
+                        <h5>目前有n位，尚缺n位助養主人<a href=" {{ url('/animals/sponsor/index', $dog->id) }} "
                                 class="btn button-addyin">加入助養</a>
                         </h5>
                     </div>
                 @endforeach
-            @endif
+            </div>
+        @else
+           <div class="row">
+                <div class="col-12 title_1">NEWS<span class="title_2">外星人</span></div>
+                <div class="col title_3">LIST TRIBE</div>
+            </div>
+            <div class="row">
 
-
-        </div>
+                @foreach ($lists as $list)
+                    <div class="col-4"><img src="{{ asset($list->pic) }}" class="imgW">
+                        <h4> {{ $list->remark }} </h4><br>
+                        <H6> {{ $list->aniname }} </H6><br>
+                        <br>
+                        <h5>目前有n位，尚缺n位助養主人<a href=" {{ url('/animals/sponsor/index', $list->id) }} "
+                                class="btn button-addyin">加入助養</a>
+                        </h5>
+                    </div>
+                @endforeach
+            </div>
+        @endif
 
     </div>
 @endsection
